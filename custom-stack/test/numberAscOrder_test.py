4e3a52d8f8a2ebb5adff19895c83218adc241f0a
caio <caiovitordias1@gmail.com>
2025-05-15 22:28:57 -0300

    passando testes para seguir o modelo de mock

--- custom-stack/test/numberAscOrder_test.py
+++ custom-stack/test/numberAscOrder_test.py
@@ -1,39 +1,56 @@
 from src.NumberAscOrder import *
 import pytest
+from unittest.mock import MagicMock, call, patch
 import random
 
 def test_6posicoescom6numSorteados():
     
-    cut = NumberAscOrder(6)
-
-    numerosSorteados = [10,30,50,60,5,2]
-
-
-    for numero in numerosSorteados:
-        cut.sortearNUmero(numero)
-
-    numerosOrdenados = cut.sort()
-
-    assert numerosOrdenados == sorted(numerosSorteados)
-
-def test_6posicoescom6numsorteadosAleatorios():
-
-    cut = NumberAscOrder(6)
-
-
-    numerosSorteio = [random.randint(0,100) for x in range(6)]
-
-    for numero in numerosSorteio:
-        cut.sortearNUmero(numero)
+    #Gerando mockk
+    mock_stack = MagicMock()
     
-    numerosOrdenados = cut.sort()
+    
+    mock_stack.is_empty.return_value = False
+    # valores estaticos na listaa
+    
+    mock_stack.elements = [10, 30, 50, 60, 5, 2]
 
-    assert numerosOrdenados == sorted(numerosSorteio)
+    # Cria a instância de NumberAscOrder e injeta o mock
+    cut = NumberAscOrder(6)
+    cut.pilha = mock_stack  # substitui a pilha real pelo mock
+
+    #metodo de ordenação
+    resultado = cut.sort()
+
+    # Valida se o resultado está ordenado corretamente
+    assert resultado == sorted(mock_stack.elements)
+    mock_stack.is_empty.assert_called_once()
+
+# def test_6posicoescom6numsorteadosAleatorios():
+
+#     cut = NumberAscOrder(6)
+
+
+#     numerosSorteio = [random.randint(0,100) for x in range(6)]
+
+#     for numero in numerosSorteio:
+#         cut.sortearNUmero(numero)
+    
+#     numerosOrdenados = cut.sort()
+
+#     assert numerosOrdenados == sorted(numerosSorteio)
 
 
 
 def test_pilhavazia():
+
+    mock_stack = MagicMock()
+    mock_stack.is_empty.return_value = True  # forçar pilha vazia
+    mock_stack.elements = []
+
     cut = NumberAscOrder(6)
+    cut.pilha = mock_stack
 
     with pytest.raises(ValueError, match="erro, pilha vazia"):
         cut.sort()
+
+    mock_stack.is_empty.assert_called_once()
